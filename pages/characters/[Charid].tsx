c330ed276ce6183ba6d555a933f2f51daea27cc1
electron-space <6634301+electron-space@users.noreply.github.com>
2022-05-16 19:58:59 -0400

    Statically attempting to render to multiple pages.

---
+++ pages/characters/[Charid].tsx
@@ -0,0 +1,32 @@
+import { GetStaticPaths } from 'next';
+import { GetCharacterResutls, Character } from '../../types/types';
+
+function CharPage({ character }: { character: Character }) {
+	return <div>{character.name}</div>;
+}
+
+export async function getStaticPaths() {
+	const res = await fetch('https://rickandmortyapi.com/api/character');
+	const { results }: GetCharacterResutls = await res.json();
+
+	return {
+		paths: results.map((character) => {
+			return { params: { id: character.id.toString() } };
+		}),
+		fallback: true,
+	};
+}
+
+export async function getStaticProps({ params }: { params: { id: string } }) {
+	const res = await fetch(
+		`https://rickandmortyapi.com/api/character/${params.id}`
+	);
+	const character = res.json();
+	return {
+		props: {
+			character,
+		},
+	};
+}
+
+export default CharPage;
