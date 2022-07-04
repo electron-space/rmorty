acaa1cf037f4aeb3f420ffce2c4ea0b0b044ab39
electron-space <6634301+electron-space@users.noreply.github.com>
2022-07-04 19:43:26 -0400

    found page without a React Component as default export in pages/paginate

--- pages/paginate.ts
+++ pages/paginate.ts
@@ -1,15 +1,15 @@
-//@ts-nocheck
-import type { GetStaticProps } from 'next';
-import { GetCharacterResults, Info } from '../types/types';
-import ReactPaginate from 'react-paginate';
+// //@ts-nocheck
+// import type { GetStaticProps } from 'next';
+// import { GetCharacterResults, Info } from '../types/types';
+// import ReactPaginate from 'react-paginate';
 
-export async function getStaticProps() {
-	const res = await fetch(`https://rickandmortyapi.com/api/character/`);
-	const info = await res.json();
+// export async function getStaticProps() {
+// 	const res = await fetch(`https://rickandmortyapi.com/api/character/`);
+// 	const info = await res.json();
 
-	return {
-		props: {
-			info,
-		},
-	};
-}
+// 	return {
+// 		props: {
+// 			info,
+// 		},
+// 	};
+// }
